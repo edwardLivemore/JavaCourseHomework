e2f49ab54ba7740e0c7bc7fd9f7e556cb154146d
zhanxiang <zhanxiang@chinadep.com>
2021-12-17 18:06:48 +0800

    Week7 homework10

---
+++ week7_mysql/homework10/multids_v2/src/main/java/com/edward/multids_v2/MultidsV2Application.java
@@ -0,0 +1,13 @@
+package com.edward.multids_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultidsV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultidsV2Application.class, args);
+	}
+
+}
---
+++ week7_mysql/homework10/multids_v2/src/test/java/com/edward/multids_v2/MultidsV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edward.multids_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultidsV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
