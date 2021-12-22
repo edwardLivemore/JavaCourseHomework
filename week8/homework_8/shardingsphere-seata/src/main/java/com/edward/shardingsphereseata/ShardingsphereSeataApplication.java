297c4c86e27dc542f91a94e17a010553f5a0e225
zhanxiang <zhanxiang@chinadep.com>
2021-12-22 17:58:41 +0800

    shardingsphere seata init

---
+++ week8/homework_8/shardingsphere-seata/src/main/java/com/edward/shardingsphereseata/ShardingsphereSeataApplication.java
@@ -0,0 +1,13 @@
+package com.edward.shardingsphereseata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShardingsphereSeataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShardingsphereSeataApplication.class, args);
+	}
+
+}
---
+++ week8/homework_8/shardingsphere-seata/src/main/java/com/edward/shardingsphereseata/modules/order/service/OrderService.java
@@ -0,0 +1,16 @@
+package com.edward.shardingsphereseata.modules.order.service;
+
+import com.edward.shardingsphereseata.modules.order.model.Order;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author eagle
+ * @since 2021-12-22
+ */
+public interface OrderService extends IService<Order> {
+
+}
