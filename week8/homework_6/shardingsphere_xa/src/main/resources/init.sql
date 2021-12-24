791c8ea105f6803a11de3162a1fbdae94a8d7610
edward <optihack@163.com>
2021-12-24 21:10:17 +0800

    Week8 homework

--- week8/homework_6/shardingsphere_xa/src/main/resources/init.sql
+++ week8/homework_6/shardingsphere_xa/src/main/resources/init.sql
@@ -1,7 +1,7 @@
 CREATE SCHEMA ds0;
 
 DROP TABLE IF EXISTS ds0.`platform_order`;
-CREATE TABLE `platform_order`  (
+CREATE TABLE ds0.`platform_order`  (
   `id` bigint(0) NOT NULL COMMENT '主键',
   `code` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '订单编号',
   `user_id` bigint(0) NOT NULL COMMENT '用户id',
@@ -14,7 +14,7 @@ CREATE TABLE `platform_order`  (
 CREATE SCHEMA ds1;
 
 DROP TABLE IF EXISTS ds1.`platform_order_item`;
-CREATE TABLE `platform_order_item`  (
+CREATE TABLE ds1.`platform_order_item`  (
   `id` bigint(0) NOT NULL COMMENT '主键',
   `order_id` bigint(0) NOT NULL COMMENT '订单编号',
   `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '商品名称',
