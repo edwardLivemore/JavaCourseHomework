8e1de2018f6dd9c9de4ca17dd3958a8671e2c7b1
edward <optihack@163.com>
2022-01-02 12:06:36 +0800

    homework 7

---
+++ week9_rpc/homework7/hmily_dubbo_demo/src/test/java/com/edward/hmily_dubbo_demo/HmilyDubboDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edward.hmily_dubbo_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HmilyDubboDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
